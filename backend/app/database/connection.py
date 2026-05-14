c732bfc38fda29e904be953b37b3f8e74b2eb1d5
Nathaniel Swearingen <nathaniel@insyt.pro>
2026-05-14 11:52:24 -0400

    Add Alembic migrations

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
 
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "insyt_capture")
